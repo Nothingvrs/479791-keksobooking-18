665957e8ae514e57580d00c5a10b2787650f9c14
Nothingvrs <b00M_vrs@mail.ru>
2019-09-24 01:51:36 +0300

    Не работает

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png' ,'img/avatars/user07.png', 'img/avatars/user08.png'];
+var AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
 var TITLE = ['Сдаю квартиру', 'Продаю квартиру', 'Квартира с ремонтом', 'Квартира в черновой отделке'];
 var TYPE = ['palace', 'flat', 'house', 'bungalo'];
 var CHECK = ['12:00', '13:00', '14:00'];
@@ -8,11 +8,7 @@ var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditio
 var DESCRIPTION = ['Описание'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var emptyArray = [];
-var mapPins = [];
-var author = {};
-var offer = {};
-var location = {};
-var mapOverlay = document.querySelector('.map__pins')
+var mapOverlay = document.querySelector('.map__pins');
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 
@@ -22,36 +18,22 @@ var getRandomElement = function (arr) {
 };
 
 var getRandomNumber = function (min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
-}
+  return Math.floor(Math.random() * (max - min)) + min;
+};
 
 var createRandomObject = function (collection) {
-	var newList = [];
-	for (var i = 0; i < getRandomNumber(1, collection.length); i++) {
-		newList[i] = collection[i];
-	}
-	return newList;
+  var newList = [];
+  for (var i = 0; i < getRandomNumber(1, collection.length); i++) {
+    newList[i] = collection[i];
+  }
+  return newList;
 };
 
 var generateMoki = function (count) {
-	for (var i = 0; i <= count; i++) {
-		var moki = {author: {avatar: getRandomElement(AVATAR)},
-      offer: {
-      title: getRandomElement(TITLE),
-      address: getRandomNumber(0, 1000) + ', ' + getRandomNumber(0, 1000),
-      price: getRandomNumber(10000000, 3000000),
-      type: getRandomElement(TYPE),
-      rooms: getRandomNumber(1, 5),
-      guests: getRandomNumber(1, 4),
-      checkin: getRandomElement(CHECK),
-      checkout: getRandomElement(CHECK),
-      features: createRandomObject(FEATURES),
-      description: getRandomElement(DESCRIPTION),
-      photos: createRandomObject(PHOTOS)
-    }, location: {x: getRandomNumber(0, 1200), y: getRandomNumber(130, 630)}};
-		emptyArray.push(moki);
-  }
-	return emptyArray
+  for (var i = 0; i <= count; i++) {
+    var moki = {author: {avatar: getRandomElement(AVATAR)}, offer: {title: getRandomElement(TITLE), address: getRandomNumber(0, 1000) + ', ' + getRandomNumber(0, 1000), price: getRandomNumber(10000000, 3000000), type: getRandomElement(TYPE), rooms: getRandomNumber(1, 5), guests: getRandomNumber(1, 4), checkin: getRandomElement(CHECK), checkout: getRandomElement(CHECK), features: createRandomObject(FEATURES), description: getRandomElement(DESCRIPTION), photos: createRandomObject(PHOTOS)}, location: {x: getRandomNumber(0, 1200), y: getRandomNumber(130, 630)}};
+    emptyArray.push(moki);
+  } return emptyArray;
 };
 
 console.log(emptyArray);
@@ -60,7 +42,7 @@ console.log(emptyArray);
 var renderMapPin = function (collection, count) {
   var fragment = document.createDocumentFragment();
   var pinTemplate = document.querySelector('#pin')
-    .content
+    .content;
   for (var i = 0; i < count; i++) {
     var mapPin = pinTemplate.cloneNode(true);
     var pinImageElement = mapPin.querySelector('img');
