118fabb51a09b8cf306e21f17a5e76bfe636b1fa
Nothingvrs <b00M_vrs@mail.ru>
2019-09-24 00:21:13 +0300

    Рабочая версия

--- js/main.js
+++ js/main.js
@@ -7,7 +7,14 @@ var CHECK = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 var DESCRIPTION = ['Описание'];
 var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var array = [];
+var emptyArray = [];
+var mapPins = [];
+var author = {};
+var offer = {};
+var location = {};
+var mapOverlay = document.querySelector('.map__pins')
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
 
 var getRandomElement = function (arr) {
   var rand = Math.floor(Math.random() * arr.length);
@@ -26,63 +33,46 @@ var createRandomObject = function (collection) {
 	return newList;
 };
 
-var createRandomOffer = function () {
-    var offer = {title: getRandomElement(TITLE),
-    	address: getRandomNumber(0, 1000) + ', ' + getRandomNumber(0, 1000),
-    	price: getRandomNumber(10000000, 3000000),
-    	type: getRandomElement(TYPE),
-    	rooms: getRandomNumber(1, 5),
-    	guests: getRandomNumber(1, 4),
-    	checkin: getRandomElement(CHECK),
-    	checkout: getRandomElement(CHECK),
-    	features: createRandomObject(FEATURES),
-    	description: getRandomElement(DESCRIPTION),
-    	photos: createRandomObject(PHOTOS)
- 	}
- 	return offer
-};
-
-var createRandomAuthor = function () {
-	var author = {avatar: getRandomElement(AVATAR)
- 	}
- 	return author
-};
-
-var createRandomLocation = function () {
-	var locationHouse = {
-		locationx: getRandomNumber(0, 1200),
-		locationy: getRandomNumber(130, 630)
-	}
-	return locationHouse
-};
-
 var generateMoki = function (count) {
 	for (var i = 0; i <= count; i++) {
-		var author = createRandomAuthor();
-		var offer = createRandomOffer();
-		var location = createRandomLocation();
-		array.push({author, offer, location})
-	}
-	return array
+		var moki = {author: {avatar: getRandomElement(AVATAR)},
+      offer: {
+      title: getRandomElement(TITLE),
+      address: getRandomNumber(0, 1000) + ', ' + getRandomNumber(0, 1000),
+      price: getRandomNumber(10000000, 3000000),
+      type: getRandomElement(TYPE),
+      rooms: getRandomNumber(1, 5),
+      guests: getRandomNumber(1, 4),
+      checkin: getRandomElement(CHECK),
+      checkout: getRandomElement(CHECK),
+      features: createRandomObject(FEATURES),
+      description: getRandomElement(DESCRIPTION),
+      photos: createRandomObject(PHOTOS)
+    }, location: {x: getRandomNumber(0, 1200), y: getRandomNumber(130, 630)}};
+		emptyArray.push(moki);
+  }
+	return emptyArray
+};
+
+console.log(emptyArray);
+
+
+var renderMapPin = function (collection, count) {
+  var fragment = document.createDocumentFragment();
+  var pinTemplate = document.querySelector('#pin')
+    .content
+  for (var i = 0; i < count; i++) {
+    var mapPin = pinTemplate.cloneNode(true);
+    var pinImageElement = mapPin.querySelector('img');
+    var pinBody = mapPin.querySelector('.map__pin');
+    pinImageElement.src = emptyArray[i].author.avatar;
+    pinBody.style.left = emptyArray[i].location.x;
+    pinBody.style.top = emptyArray[i].location.y;
+    pinImageElement.alt = emptyArray[i].offer.title;
+    fragment.appendChild(mapPin);
+  }
+  mapOverlay.appendChild(fragment);
 };
 
 generateMoki(7);
-console.log(array);
-
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
-var renderMapPin = function (data, count) {
-var pinTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
-  for (var i = 0; i < count; i++) {
-    var mapPin = pinTemplate.cloneNode(true);
-    mapPin.querySelector('.map__pin').children.src = array[i].author.avatar;
-    mapPin.querySelector('.map__pin').style.left = array[i].location.locationx;
-    mapPin.querySelector('.map__pin').style.right = array[i].location.locationy;
-    mapPin.querySelector('.map__pin').children.alt.textContent = array[i].offer.description;
-  }
-};
-
-renderMapPin(array, 8);
+renderMapPin(emptyArray, 7);
