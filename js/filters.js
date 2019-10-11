5570faf123101f7445be9c99eea95c2f660d5e90
Nothingvrs <b00M_vrs@mail.ru>
2019-10-11 23:58:41 +0300

    Перламутровые пуговицы 7

--- js/filters.js
+++ js/filters.js
@@ -1,25 +1,25 @@
 'use strict';
 (function () {
-  var filterType = document.getElementById('housing-type');
-  var pinsOnMap = document.querySelectorAll('.map__pin');
+  var filterType = document.querySelector('[name = housing-type]');
+
   var cleanMap = function () {
+    var pinsOnMap = document.querySelectorAll('.map__pin');
     for (var i = 1; i < pinsOnMap.length; i++) {
       window.pin.mapOverlay.removeChild(pinsOnMap[i]);
     }
   };
 
   var filteredType = function (value) {
-    cleanMap();
     var filteredPins = window.pins.filter(function (pin) {
       return pin.offer.type === value;
     });
     window.pin.renderPins(filteredPins);
   };
 
-  var onFiltered = function () {
-    var index = pinsOnMap.options.selectedIndex;
-    return pinsOnMap.options[index].value;
-  };
-
-  filterType.addEventListener('change', filteredType(onFiltered));
+  filterType.addEventListener('change', function () {
+    var index = filterType.options.selectedIndex;
+    var selectedValue = filterType.options[index].value;
+    cleanMap();
+    filteredType(selectedValue);
+  });
 })();
